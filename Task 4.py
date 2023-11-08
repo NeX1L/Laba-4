27d69c8395bedf7598f401dcf6590b16570847e0
Egor N <egoranik0110@gmail.com>
2023-11-08 12:35:57 +0300

    pointer3

--- Task 4.py
+++ Task 4.py
@@ -4,21 +4,21 @@ class Book:
         self.author = author
         self.year = year
 
-    def display_info(self):
+    def display(self):
         return f"Книга: {self.title}, Автор: {self.author}, Год издания: {self.year}"
 
     @staticmethod
-    def is_leap_year(year):
+    def visikosny(year):
         return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
     @classmethod
-    def get_genre(cls):
+    def janr(cls):
         return "Роман"
 
+if __name__ == '__main__':
+    book1 = Book("Война и мир", "Лев Толстой", 1869)
+    print(book1.display())
+    year = 2000
+    print(f"Год {year} {'високосный' if Book.visikosny(year) else 'не високосный'}")
 
-book1 = Book("Война и мир", "Лев Толстой", 1869)
-print(book1.display_info())
-year = 2000
-print(f"Год {year} {'високосный' if Book.is_leap_year(year) else 'не високосный'}")
-
-print(f"Жанр книги: {Book.get_genre()}")
+    print(f"Жанр книги: {Book.janr()}")
